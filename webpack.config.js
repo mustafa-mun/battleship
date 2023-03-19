224c2639fa5baa79e44f76fde10590dd724c37dc
Mustafa <mustafamcm9@gmail.com>
2023-03-19 13:19:08 +0300

    configuration files

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+  plugins: [new ESLintPlugin()],
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+  },
+};
