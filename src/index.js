4068c4800806105360aebcdc79301cdb51af4708
Mustafa <mustafamcm9@gmail.com>
2023-03-20 21:54:12 +0300

    under maintance

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import "./style.scss";
 import createGameBoards from "./modules/dom/create-board/createBoard";
 import GameBoard from "./modules/gameboard/gameBoard";
-import Ship from "./modules/ship/shipFactory";
+// import Ship from "./modules/ship/shipFactory";
 import reflectShips from "./modules/dom/ships-dom/reflectShips";
 import reflectShipDamage from "./modules/dom/ships-dom/shipDamage";
 import * as PlayerController from "./modules/player/player";
 import * as Elements from "./modules/dom/elements/elements";
 
-Elements.theme.play();
-Elements.theme.volume = 0.1;
-
 let gameOver = false;
 
 createGameBoards();
 
+// Elements.theme.play();
+// Elements.theme.volume = 0.1;
+
 const aiSquares = document.getElementsByClassName("ai-square");
+const playerSquares = document.getElementsByClassName("player-square");
 const playerBoard = GameBoard();
 const aiBoard = GameBoard();
 
@@ -27,24 +28,45 @@ if (!gameOver) {
 
   for (let i = 0; i < aiSquares.length; i += 1) {
     aiSquares[i].addEventListener("click", (event) => {
-      console.log(event.target);
+      console.log(event.target.id);
       Elements.cannonHit.currentTime = 0;
       Elements.cannonHit.play();
     });
   }
 
-  playerBoard.placeShip(1, 1, Ship(4, 3));
-  playerBoard.placeShip(3, 3, Ship(1, 4));
-  playerBoard.placeShip(6, 5, Ship(1, 2));
-  playerBoard.placeShip(7, 8, Ship(1, 3));
-  playerBoard.placeShip(10, 2, Ship(3, 1));
-  playerBoard.placeShip(10, 10, Ship(1, 4));
+  for (let i = 0; i < playerSquares.length; i += 1) {
+    playerSquares[i].addEventListener("dragover", (event) => {
+      event.preventDefault();
+    });
+    playerSquares[i].addEventListener("drop", (event) => {
+      event.preventDefault();
+      Elements.twoSquareFleet.addEventListener(
+        "dragend",
+        (e) => {
+          console.log(e.target.id);
+        },
+        { once: true }
+      );
+      if (!event.target.style.backgroundColor) {
+        console.log(event.target);
+      }
+    });
+  }
+
+  // playerBoard.placeShip(1, 1, Ship(4, 3));
+  // playerBoard.placeShip(3, 3, Ship(1, 4));
+  // playerBoard.placeShip(6, 5, Ship(1, 2));
+  // playerBoard.placeShip(7, 8, Ship(1, 3));
+  // playerBoard.placeShip(10, 2, Ship(3, 1));
+  // playerBoard.placeShip(10, 10, Ship(1, 4));
   // Reflect ships to dom
   reflectShips(playerBoard.shipsArray);
   // // Random ai move to player gameBoard
   const ai = PlayerController.AIplayer();
   const randomAIMove = ai.randomMove();
   playerBoard.receiveAttack(randomAIMove[0], randomAIMove[1]);
+  // Elements.cannonHit.currentTime = 0;
+  // Elements.cannonHit.play();
   // Reflect ship damage to dom
   reflectShipDamage(playerBoard.shipsArray);
 }
--- src/modules/dom/elements/elements.js
+++ src/modules/dom/elements/elements.js
@@ -1,2 +1,3 @@
 export const cannonHit = document.getElementById("cannon-hit");
 export const theme = document.getElementById("battle-theme");
+export const twoSquareFleet = document.getElementById("twoSquareFleet")
